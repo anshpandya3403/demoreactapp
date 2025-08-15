a201282a137469d0d15b98c5f77f97d6654f8d80
anshpandya3403 <153827964+anshpandya3403@users.noreply.github.com>
2025-08-15 23:06:26 +0530

    Update server.js
    Remove sensitive info

--- src/Server/server.js
+++ src/Server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const dbURI = 'mongodb+srv://anshpandya3403:hSpBwh08iGEuUDeL@messages.sfzxxpr.mongodb.net/?retryWrites=true&w=majority&appName=Messages';
+const dbURI = 'mongodb+srv://anshpandya3403:';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
